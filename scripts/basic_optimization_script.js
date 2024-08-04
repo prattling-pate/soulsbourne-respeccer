dba01e378cda119128ffd6d651e6789e695b3e03
Kacper Lubczynski <kacperlubczynski564@gmail.com>
2024-08-04 11:35:33 +0100

    added a prototype for basic skill optimization

--- scripts/basic_optimization_script.js
+++ scripts/basic_optimization_script.js
@@ -1,9 +1,9 @@
 // function gets the ranking of basic skills from the html of the webpage
 // assigns each scale a float which emphasizes how many things to add to it (or something)
 function getRankingOfBasicSkills() {
-    let basicSkills = {};
+    let basicSkills = [];
     $(".skillMiscRank").each(function() {
-        basicSkills[$(this).attr('id')]=$(this).val();
+        basicSkills.push($(this).val());
     });
     return basicSkills;
 }
@@ -12,7 +12,34 @@ function getLevelsForBasicSkills() {
     return getNumberFromElement("miscSkillInput");
 }
 
+function sum(list) {
+    return list.reduce((a, b) => a + b, 0);
+}
+
+function assignBasicSkills(defaultSkillPoints, basicSkillsScale, levels) {
+    let solution = [...defaultSkillPoints];
+    const total = sum(basicSkillsScale);
+    for (let i=0; i<solution.length; i++) {
+        solution[i] += Math.round(basicSkillsScale[i] / total * levels);
+    }
+    return solution;
+}
+
+function outputBasicSkills(solution) {
+    $(".output.misc").each(function() {
+        $(this).val(solution.shift());
+    });
+}
+
+function getBasicDefaultSkillPoints() {
+    return getValueListFromDivClass("initialStats.misc", asNumber=true);
+}
+
 $("#respecButton").on("click", function() {
     let basicSkillsScale = getRankingOfBasicSkills();
     basicSkillsScale = convertLetterGradesToFloat(basicSkillsScale); 
+    const levels = getLevelsForBasicSkills();
+    const defaultSkillPoints = getBasicDefaultSkillPoints();
+    const solution = assignBasicSkills(defaultSkillPoints, basicSkillsScale, levels);
+    outputBasicSkills(solution);
 })
